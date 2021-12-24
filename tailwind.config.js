1c3524f9fba9f98ff62d9d691d58f62d23001d61
dotNate <somersetnathaniel@gmail.com>
2021-12-24 16:14:12 +0000

    removed boilerplate code and added base colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
+    colors: {
+      primary: "#7C5DFA",
+      primary_active: "#9277FF",
+      warning: "#EC5757",
+      warning_active: "#9277FF",
+      bg_light: "#F8F8FB",
+      bg_dark: "#141625",
+      bg_nav: "#373B53",
+    },
     extend: {},
   },
   plugins: [],
