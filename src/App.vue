0b720eba0875641b596752cda42010e71bf662d6
dotNate <somersetnathaniel@gmail.com>
2021-12-25 10:30:36 +0000

    added basic css and markup for the nav; removed tailwindcss and deps from project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
+    <router-link to="/">
+      <div id="home">
+        <img src="@/assets/logo.svg" alt="logo" />
+        <div></div>
+      </div>
+    </router-link>
+    <div class="utilities">
+      <div id="theme-select">
+        <img src="@/assets/icon-moon.svg" alt="select dark theme" />
+      </div>
+      <div id="avatar">
+        <img src="@/assets/image-avatar.jpg" alt="profile picture" />
+      </div>
+    </div>
   </div>
   <router-view />
 </template>
+
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (min-width: 1280px) {
+  #app {
+    flex-direction: row;
+  }
+}
+</style>
